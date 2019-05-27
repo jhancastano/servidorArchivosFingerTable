ba3f5c3149277157ed32bac8967b7906c1cec75c
jhon andrey castaño quintero <jhonandreycastanoquintero@jhon.local>
2019-05-27 17:53:05 -0500

    v1.2

--- nodo/nodo.py
+++ nodo/nodo.py
@@ -16,24 +16,25 @@ def hash(idnodo):
 	print(cadena)
 	return cadena
 
-def main():# 1arg=nodoID, 2arg=idsucesor 2arg=puerto sucesor
-	if(len(sys.argv)==4):
+def changeSocket(socket,nodo,conexion):
+	pass
+
+
+def main():# 1arg=nodoID, 2ipnodo, 3puerto nodo, 4arg=idsucesor 5arg=puerto sucesor
+	if(len(sys.argv)==6):
 		print(sys.argv[1])
 		nodoID = hash(sys.argv[1])
-		conexion = {'Sucesor':'tcp://'+sys.argv[2]+':'+sys.argv[3] ,'Predecesor':'null'}
-		print('hola')						
-		print(conexion['Sucesor'])
+		nodosConectados = {'Sucesor':{'id':'null','name': 'tcp://'+sys.argv[4]+':'+sys.argv[5]} ,'Predecesor':{'id':'null','name':'null'}}
 		context = zmq.Context()
 		sock = context.socket(zmq.DEALER)
 		sock.identity = nodoID.encode('utf8')
-		sock.connect(conexion['Sucesor'])
-
-		msg = json.dumps(conexion)
-
+		sock.connect(nodosConectados['Sucesor']['name'])
+		msg = json.dumps(nodosConectados)
 		printD(msg.encode('utf8'),nodoID.encode('utf8'))
-
+		sockrouter = context.socket(zmq.ROUTER)
+		sockrouter.bind("tcp://*:"+sys.argv[3])
 	else:
-		print('se ejecuta con 3 argv')
+		print('se ejecuta con 6 argv')
 
 
 
